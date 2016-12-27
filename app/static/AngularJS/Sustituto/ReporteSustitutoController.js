47acb90a89e335658c2711dbdefb87ee223f2fbb
Candi1 <aperez@bism.com.mx>
2016-12-27 17:01:11 -0600

    reporte

--- app/static/AngularJS/Sustituto/ReporteSustitutoController.js
+++ app/static/AngularJS/Sustituto/ReporteSustitutoController.js
@@ -1,46 +1,37 @@
 registrationModule.controller('ReporteSustitutoController', function (MarkerCreatorService,$scope, $modal, $route, $rootScope, $location, localStorageService, alertFactory, globalFactory, ordenServicioRepository, uploadRepository, ordenPorCobrarRepository, commonService, ordenAnticipoRepository, trabajoRepository ) {
 	
 	
+MarkerCreatorService.createByCoords(19.4353367, -99.1379815, function (marker) {
+        marker.options.labelContent = 'Posición';
+        $scope.autentiaMarker = marker;
+    });
+    
+    $scope.map = {
+        center: {
+            latitude: $scope.autentiaMarker.latitude,
+            longitude: $scope.autentiaMarker.longitude
+        },
+        zoom: 12,
+        markers: [],
+        control: {},
+        options: {
+            scrollwheel: false
+        }
+    }
 
-	 MarkerCreatorService.createByCoords(19.4353367, -99.1379815, function (marker) {
-            marker.options.labelContent = 'Posición';
-            $scope.autentiaMarker = marker;
+    $scope.map.markers.push($scope.autentiaMarker);
+
+    $scope.addCurrentLocation = function () {
+        MarkerCreatorService.createByCurrentLocation(function (marker) {
+            marker.options.labelContent = 'Usted se encuentra Aqui';
+            $scope.map.markers.push(marker);
+            refresh(marker);
         });
-        
-        $scope.map = {
-            center: {
-                latitude: $scope.autentiaMarker.latitude,
-                longitude: $scope.autentiaMarker.longitude
-            },
-            zoom: 12,
-            markers: [],
-            control: {},
-            options: {
-                scrollwheel: false
-            }
-        }
+    }
 
-        $scope.map.markers.push($scope.autentiaMarker);
-
-        $scope.addCurrentLocation = function () {
-            MarkerCreatorService.createByCurrentLocation(function (marker) {
-                marker.options.labelContent = 'Usted se encuentra Aqui';
-                $scope.map.markers.push(marker);
-                refresh(marker);
-            });
-        }
-        /* $scope.addAddress = function() {
-            var address = $scope.address;
-            if (address !== '') {
-                MarkerCreatorService.createByAddress(address, function(marker) {
-                    $scope.map.markers.push(marker);
-                    refresh(marker);
-                });
-            }
-        }*/
-        function refresh(marker) {
-            $scope.map.control.refresh({latitude: marker.latitude,
-                longitude: marker.longitude});
+        $scope.psicion = function (){
+            debugger;
+             $('#MapaModal').appendTo("body").modal('show');
         }
 
 
